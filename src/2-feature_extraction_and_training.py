59b4ade8ce1bb9327434f94b95e653c618bbfe49
gramos <gramos@DESKTOP-BI03DP2.localdomain>
2018-04-20 13:49:15 +0000

    Feature extraction ready!

--- src/2-feature_extraction_and_training.py
+++ src/2-feature_extraction_and_training.py
@@ -11,6 +11,8 @@ from skimage import morphology
 from skimage import img_as_float
 from skimage import img_as_uint
 from sklearn.linear_model import LogisticRegression
+import scipy
+import pywt
 
 
 classes = ['Nex7','HTC-1-M7','iP4s','iP6','LG5x','MotoMax','MotoNex6','MotoX','GalaxyN3','GalaxyS4']
@@ -45,11 +47,6 @@ def process_based_on_noise(noised_image):#,device):
     # May be useful to change disk size (maybe?)
     # ValueError: Images of type float must be between -1 and 1.
     #noised_image = img_as_float(noised_image)
-    try:
-        noised_image = img_as_uint(noised_image)
-    except:
-        print('=============erro===========')
-        print(noised_image)
     m_mean_rank = mean_each(noised_image)
     m_entropy_rank = entropy_each(noised_image)
 
@@ -73,13 +70,34 @@ def extract_features_from_image(image):
     feature_list = []
     # Get median version of the image
     median_version = image - median_each(image)
-    feature_list = process_based_on_noise(median_version)
-    # Get bilateral version of the image
-    bilateral_version = image - restoration.denoise_bilateral(image)
-    feature_list.extend(process_based_on_noise(bilateral_version))
-    # Get wavelet_version of the image
-    wavelet_version = image - restoration.denoise_wavelet(image)
-    feature_list.extend(process_based_on_noise(wavelet_version))
+    for i in range(0,3):
+        LL, HIGH_VALUES = pywt.dwt2(median_version[...,i],'haar')
+        for item in HIGH_VALUES:
+            data_description = scipy.stats.describe(item)
+            # Mean
+            feature_list.append(min(data_description[2]))
+            feature_list.append(max(data_description[2]))
+            feature_list.append(sum(data_description[2])/float(data_description[0]))
+            # Variance
+            feature_list.append(min(data_description[3]))
+            feature_list.append(max(data_description[3]))
+            feature_list.append(sum(data_description[3])/float(data_description[0]))
+            # Skewness
+            feature_list.append(min(data_description[4]))
+            feature_list.append(max(data_description[4]))
+            feature_list.append(sum(data_description[4])/float(data_description[0]))
+            # Kurtosis
+            feature_list.append(min(data_description[5]))
+            feature_list.append(max(data_description[5]))
+            feature_list.append(sum(data_description[5])/float(data_description[0]))
+        
+    #feature_list = process_based_on_noise(median_version)
+    # Get bilateral version of the image - NOT WORKING WELL
+    # bilateral_version = image - restoration.denoise_bilateral(image,bins=3)
+    # feature_list.extend(process_based_on_noise(bilateral_version))
+    # Get wavelet_version of the image -- IS THIS REALLY NECESSARY?
+    # wavelet_version = image - restoration.denoise_wavelet(image)
+    # feature_list.extend(process_based_on_noise(wavelet_version))
     return feature_list
     
 def main():
@@ -88,6 +106,9 @@ def main():
     global number_of_samples
     
     lr_model = LogisticRegression()
+
+    features = []
+    devices = []
 
     for device in classes:
         for i in range(1,11):
@@ -102,18 +123,19 @@ def main():
             # =======================================================
             print('Processing image '+file_name)
             feature_list = extract_features_from_image(image)
-
+            features.append(feature_list)
+            devices.append(device)
             
             # Get LBP version of the image - NOT INTERESTING, SAY THIS: are the binary representation of the texture fluctuation around each point, LOOK WHAT IS LBP 4 REAL
             # lbp_version = image - lbp_each(image)
 
             # Training the LogisticRegression
-            lr_model.fit([feature_list],[device])
+    lr_model.fit(features,devices)
 
     # Single test
-    test_image = io.imread('../input/(GalaxyN3)1.tiff')
-    test_image = extract_features_from_image(test_image)
-    print(lr_model.predict(test_image))
+    test_images = io.imread('../input512/(iP6)1.tiff')
+    test_images = extract_features_from_image(test_images)
+    print(lr_model.predict([test_images]))
         
 if __name__ == '__main__':
     main()        
